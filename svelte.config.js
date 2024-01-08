e3610a0d7fcbd39873bae9a9552a75324cce3994
Ryan Williams <ryan.williams@signalwire.com>
2024-01-08 14:49:20 -0800

    build: Add vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -16,3 +17,5 @@ const config = {
 };
 
 export default config;
+
+// export default {	kit: {		adapter: adapter({			// see below for options that can be set here		})	}};
