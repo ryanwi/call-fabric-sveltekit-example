5a89800c8d0a1a1a9658ded600fa0253d65ebd49
Ryan Williams <ryan.williams@signalwire.com>
2024-01-08 13:54:22 -0800

    feat: Implement navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,65 +1,170 @@
 <script lang="ts">
-  import { page } from "$app/stores"
-  import "../app.css";
+  import { page } from '$app/stores';
+  import '../app.css';
 </script>
 
-<div class="navbar bg-base-100">
-  <div class="flex-none">
-    <button class="btn btn-square btn-ghost">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-    </button>
-  </div>
+<div class="navbar bg-neutral text-neutral-content">
   <div class="flex-1">
-    <a class="btn btn-ghost text-xl" href="/">Call Fabric Demo</a>
+    <a href="/" class="btn btn-neutral text-xl">Call Fabric Demo</a>
   </div>
-  <div class="flex-none">
-    <button class="btn btn-square btn-ghost">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
-    </button>
-  </div>
+  {#if $page.data.session}
+    <div class="flex-0">
+      <div class="">
+        <a href="/rooms" class="btn btn-ghost" data-sveltekit-preload-data="off">Rooms</a>
+      </div>
+      <div class="">
+        <a href="/calls" class="btn btn-ghost" data-sveltekit-preload-data="off">Calls</a>
+      </div>    
+      <div class="dropdown dropdown-end">
+        <div class="flex-none">
+          <button class="btn btn-square btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="inline-block w-5 h-5 stroke-current"
+              ><path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path></svg
+            >
+          </button>
+        </div>
+        <ul
+          class="menu menu-md dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 text-base-content rounded-box w-80"
+        >
+          <li>
+            <div style="display: flex; align-items: center;">
+              <div class="avatar placeholder">
+                <div class="bg-neutral text-neutral-content rounded-full w-14">
+                  <span class="text-2xl">RW</span>
+                </div>
+              </div>
+              <div>
+                {$page.data.session.user?.email}
+              </div>
+            </div>
+          </li>
+          <li></li>
+          <li>
+            <a href="/">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                />
+              </svg>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="/profile">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-5 h-5"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                />
+              </svg>
+              My Profile
+            </a>
+          </li>
+          <li>
+            <a href="/settings">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-5 h-5"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z"
+                />
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                />
+              </svg>
+              Settings
+            </a>
+          </li>
+          <li>
+            <a href="/about">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                /></svg
+              >
+              About
+            </a>
+          </li>
+          <li></li>
+          <li>
+            <a href="/auth/signout" class="button" data-sveltekit-preload-data="off">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-5 h-5"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9"
+                />
+              </svg>
+              Log Out
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  {:else}
+    <div class="flex-none gap-2">
+      <div>
+        <a href="/auth/signin" class="btn btn-ghost" data-sveltekit-preload-data="off">Sign in</a>
+      </div>
+      <div>
+        <a href="/signup" class="btn btn-primary" data-sveltekit-preload-data="off">Join</a>
+      </div>
+    </div>
+  {/if}
 </div>
 
 
-<div>
-  <header>
-    <div class="signedInStatus">
-      <p class="nojs-show loaded">
-        {#if $page.data.session}
-          {#if $page.data.session.user?.image}
-            <span
-              style="background-image: url('{$page.data.session.user.image}')"
-              class="avatar"
-            />
-          {/if}
-          <span class="signedInText">
-            <small>Signed in as</small><br />
-            <strong
-              >{$page.data.session.user?.email ??
-                $page.data.session.user?.name}</strong
-            >
-          </span>
-          <a
-            href="/auth/signout"
-            class="button"
-            data-sveltekit-preload-data="off">Sign out</a
-          >
-        {:else}
-          <span class="notSignedInText">You are not signed in</span>
-          <a
-            href="/auth/signin"
-            class="buttonPrimary"
-            data-sveltekit-preload-data="off">Sign in</a
-          >
-        {/if}
-      </p>
-    </div>
-    <nav>
-      <ul class="navItems">
-        <li class="navItem"><a href="/">Home</a></li>
-        <li class="navItem"><a href="/full">Full</a></li>
-        <li class="navItem"><a href="/minimal">Minimal</a></li>
-      </ul>
-    </nav>
-  </header>
+<div class="">
   <slot />
 </div>
