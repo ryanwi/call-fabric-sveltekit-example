9a7740ee9ee3f2f3037961084ab2f3bbbe390e41
Ryan Williams <ryan.williams@signalwire.com>
2024-01-10 11:21:48 -0800

    render a room

---
+++ src/routes/rooms/[slug]/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { browser } from '$app/environment'; 
+
+	/** @type {import('./$types').PageData} */
+	export let data;
+</script>
+
+<svelte:head>
+  <script type="text/javascript" src="https://webrtc.github.io/adapter/adapter-latest.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pako/2.1.0/pako.min.js"></script>  
+</svelte:head>
+
+<h1 class="text-4xl mb-4 mt-4 font-extrabold">Room</h1>
+
+<div>
+  <p>Data</p>
+  <p>{JSON.stringify(data)}</p>  
+</div>
+
+<div>
+  <p>Room</p>
+  <p>{JSON.stringify(data.room)}</p>  
+</div>
