a67dc3e6b9f467a8d9a046da590ce98a623f2845
Ryan Williams <ryan.williams@signalwire.com>
2024-01-13 22:13:12 -0800

    feat: set up data for SDK initialization

--- src/lib/components/Room.svelte
+++ src/lib/components/Room.svelte
@@ -1,21 +1,24 @@
-<script>
-  export let room;
-  export let sat;
-
-  const _token = sat;
-  const _host = "relay.swire.io";
-
+<script lang="ts">
   import { browser } from '$app/environment';
+  import type { FabricAddress } from '$lib/types/FabricAddress';
+  import { PUBLIC_RELAY_HOST } from '$env/static/public';
+
+  // passed as properties
+  export let room: FabricAddress;
+  export let sat: string;
+
   if (browser) {
-    console.log("sat in the browser?", sat);
-    console.log(_host, _token);
-  }  
+    const _token = sat;
+    const _host = PUBLIC_RELAY_HOST;
+    const destination = room.channels["video"];
+    console.log("This is the browser", _token, _host, destination)
+  }
 </script>
 
 <svelte:head>
-  <script type="text/javascript" src="https://webrtc.github.io/adapter/adapter-latest.js"></script>
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pako/2.1.0/pako.min.js"></script>  
-  <script type="text/javascript" src="https://unpkg.com/@signalwire/js"></script>
+  <script async type="text/javascript" src="https://webrtc.github.io/adapter/adapter-latest.js"></script>
+  <script async type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pako/2.1.0/pako.min.js"></script>  
+  <script async type="text/javascript" src="https://unpkg.com/@signalwire/js"></script>
 </svelte:head>
 
 <div>
