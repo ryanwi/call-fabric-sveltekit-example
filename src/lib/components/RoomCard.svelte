2e604a2f3bc29cd07e7dac29460d87bce7d4ecec
Ryan Williams <ryan.williams@signalwire.com>
2024-01-13 17:29:08 -0800

    feat: Add a RoomCard component

---
+++ src/lib/components/RoomCard.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let room;
+ </script>
+ 
+ <div class="card w-96 bg-base-100 shadow-xl m-6">
+  <figure><img src="https://files.signalwire.com/cantina/02e3021d-861c-49da-9741-5c811aac8657/promos/44513608-5ff4-4b84-bccc-74c15f857c8c.jpg" /></figure>
+  <div class="card-body">
+    <h2 class="card-title">{room.display_name}</h2>
+    <p>{room.name}</p>
+    <div class="card-actions justify-end">
+      <a href="/rooms/{room.name}" class="btn btn-primary">Join</a>
+    </div>
+  </div>
+</div> 
--- src/routes/rooms/+page.svelte
+++ src/routes/rooms/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import RoomCard from '$lib/components/RoomCard.svelte';
+
   /** @type {import('./$types').PageData} */
   export let data;
   // console.log('page.svelte data =', data);
@@ -9,17 +11,8 @@
 {#if data.session}
   {#if data.rooms}
     <div class="grid grid-cols-3">
-      {#each data.rooms as room}
-        <div class="card w-96 bg-base-100 shadow-xl m-6">
-          <figure><img src="https://files.signalwire.com/cantina/02e3021d-861c-49da-9741-5c811aac8657/promos/44513608-5ff4-4b84-bccc-74c15f857c8c.jpg" /></figure>
-          <div class="card-body">
-            <h2 class="card-title">{room.display_name}</h2>
-            <p>{room.name}</p>
-            <div class="card-actions justify-end">
-              <a href="/rooms/{room.name}" class="btn btn-primary">Join</a>
-            </div>
-          </div>
-        </div> 
+      {#each data.rooms as room (room.id)}
+        <RoomCard {room} />
       {/each}
     </div>
   {:else if data.error}
@@ -28,6 +21,6 @@
 {:else}
   <h1>Access Denied</h1>
   <p>
-    <a href="/auth/signin"> You must be signed in to view this page </a>
+    <a href="/auth/signin" aria-label="Sign in"> You must be signed in to view this page </a>
   </p>
 {/if}
