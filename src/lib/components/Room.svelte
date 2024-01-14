73f7e8270e955454bbf3bdf9a7a0d4e03ec55340
Ryan Williams <ryan.williams@signalwire.com>
2024-01-13 17:47:15 -0800

    feat: Create Room component

---
+++ src/lib/components/Room.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let room;
+  export let sat;
+</script>
+
+<svelte:head>
+  <script type="text/javascript" src="https://webrtc.github.io/adapter/adapter-latest.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pako/2.1.0/pako.min.js"></script>  
+</svelte:head>
+
+<div>
+  <p>Room</p>
+  <p>{JSON.stringify(room)}</p>  
+</div>
+
+<p>Token: {sat}</p>
--- src/routes/rooms/[slug]/+page.svelte
+++ src/routes/rooms/[slug]/+page.svelte
@@ -1,22 +1,17 @@
 <script>
+  import Room from '$lib/components/Room.svelte';
+
   /** @type {import('./$types').PageData} */
   export let data;
   // console.log('page.svelte data =', data);
-</script>
 
-<svelte:head>
-  <script type="text/javascript" src="https://webrtc.github.io/adapter/adapter-latest.js"></script>
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pako/2.1.0/pako.min.js"></script>  
-</svelte:head>
+  export let room = data.room;
+  let sat = data.session?.sat;
+
+  console.log("sat in the browser?", sat);
+  // yes AND it's on the server console though.
+</script>
 
 <h1 class="text-4xl mb-4 mt-4 font-extrabold">Room</h1>
 
-<div>
-  <p>Data</p>
-  <p>{JSON.stringify(data)}</p>  
-</div>
-
-<div>
-  <p>Room</p>
-  <p>{JSON.stringify(data.room)}</p>  
-</div>
+<Room {room} {sat} />
